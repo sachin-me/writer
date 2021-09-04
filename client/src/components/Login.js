8b596090fecaac5f1b1638c1f1d800e13fdc10a6
sachin-me <sachinkumartiwari363@gmail.com>
2021-09-04 18:26:21 +0530

    add email and password validation in login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import userAction from "../actions/userAction";
+import helperFunctions from "../Utility";
 import Message from "./Common/Message";
+
+const { validateEmail, validatePassword } = helperFunctions;
 
 class Login extends Component {
   state = {
@@ -10,24 +13,67 @@ class Login extends Component {
     password: "",
     message: null,
     error: null,
+    validEmail: true,
+    validPassword: true,
   };
 
   handleChange = ({ target: { name, value } }) => {
-    this.setState({
-      [name]: value,
-    });
+    this.setState(
+      {
+        [name]: value,
+      },
+      () => this.validateEmailPassword()
+    );
+  };
+
+  validateEmailPassword = () => {
+    let { email, password, validEmail, validPassword } = this.state;
+
+    this.setState(
+      {
+        validEmail: validateEmail(email),
+        validPassword: validatePassword(password),
+      },
+      () => {
+        if (email || password) {
+          if (email && !validEmail) {
+            return this.setState({
+              error: "*Enter valid email address (e.g. abc@gmail.com)",
+            });
+          }
+          if (password && !validPassword) {
+            return this.setState({
+              error:
+                "*Password must contain 4-8 characters and at least One Uppercase letter and one numeric value.",
+            });
+          } else {
+            return this.setState({
+              error: "",
+            });
+          }
+        }
+      }
+    );
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
     const { email, password } = this.state;
 
-    const payload = {
-      email,
-      password,
-    };
+    if (validateEmail(email) && validatePassword(password)) {
+      const payload = {
+        email,
+        password,
+      };
 
-    this.props.dispatch(userAction.loginUser(payload, this.handleSubmitReturn));
+      this.props.dispatch(
+        userAction.loginUser(payload, this.handleSubmitReturn)
+      );
+    } else {
+      this.setState({
+        error: "Email or Password is invalid",
+      });
+    }
   };
 
   handleSubmitReturn = (success, error) => {
