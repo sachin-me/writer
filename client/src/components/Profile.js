1c9dad9762f983580866dd6b80cd128ef96e154e
sachin-me <sachinkumartiwari363@gmail.com>
2021-09-09 10:37:00 +0530

    Fixed logout API with functionality

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,36 +1,39 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Redirect, Link } from 'react-router-dom';
-import { logout } from '../actions';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import userAction from "../actions/userAction";
 
 class Profile extends Component {
   handleClick = () => {
-    this.props.dispatch(logout(succeed => {
-      if (succeed) {
-        this.props.history.push(`/`)
-      }
-    }))
-  }
+    this.props.dispatch(
+      userAction.logout((succeed) => {
+        if (succeed) {
+          window.location.href = "/";
+        }
+      })
+    );
+  };
 
   render() {
     const { user } = this.props;
     return (
       <div className="profile-wrapper">
-        <button onClick={
-          () => (
-            (user.id) ? this.handleClick() : <Redirect to='/login' />
-          )
-        }>Click here to Logout</button>
+        <button
+          onClick={() =>
+            user._id ? this.handleClick() : <Redirect to="/login" />
+          }
+        >
+          Click here to Logout
+        </button>
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    user: state.user
-  }
-}
+    user: state.user,
+  };
+};
 
 export default connect(mapStateToProps)(Profile);
