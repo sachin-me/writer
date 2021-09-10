1a36c87a5b83d6aeca7e97b01b40580038f2588b
sachin-me <sachinkumartiwari363@gmail.com>
2021-09-10 11:21:32 +0530

    Added dist static path for prod

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "./server/views"));
