245352beb910322e121c6d5b5cf865f9faad4293
sachin-me <sachinkumartiwari363@gmail.com>
2021-09-10 11:16:01 +0530

    Add cssPath and jsPath in index.ejs

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const hash = "bundle";
 
 router.get("*", (req, res) => {
-  res.render("index");
+  const cssPath =
+    process.env.NODE_ENV == "production"
+      ? `/bundle/${hash}.css`
+      : "/static/bundle.css";
+  const jsPath =
+    process.env.NODE_ENV == "production"
+      ? `/bundle/${hash}.js`
+      : "/static/bundle.js";
+  res.render("index", { jsPath, cssPath });
 });
 
 module.exports = router;
